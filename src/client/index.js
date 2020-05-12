14f526207d87f83541da8d7ccb6fa6bbf34cb7be
parthgupta118 <guptaparth118@gmail.com>
2020-05-12 21:39:10 +0530

    Server setup

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import {checkForDate } from './js/dateChecker';
+import { checkForLocation } from './js/locationChecker';
+import { handleSubmit } from './js/formHandler';
+
+console.log(checkForDate);
+
+
+export {
+    checkForDate,
+    checkForLocation,
+    handleSubmit 
+}
