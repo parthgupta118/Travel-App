86b1602f47bcadf9b0f4a025eb7ccaf4f166a1d8
parthgupta118 <guptaparth118@gmail.com>
2020-05-15 00:38:26 +0530

    Webpack Updated

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -48,6 +47,5 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        new WorkboxPlugin.GenerateSW()
     ]
 }
